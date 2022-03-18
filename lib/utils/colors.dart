b75b8b452631fe4162b0dfd7a76a164ab853cfa4
Dylan Dabrowski <57879165+DylanDabrowski@users.noreply.github.com>
2022-03-18 16:58:29 -0400

    cleaned up main.dart, added basic file structure, and added getx dependecy to yaml file.

---
+++ lib/utils/colors.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color mainColor = Color.fromARGB(255, 87, 41, 212);
+  static const Color secondaryColor = Color.fromARGB(255, 95, 201, 178);
+  static const Color textColor = Color.fromARGB(255, 20, 20, 20);
+}
